caf840c6dfe367b14964521ec1d31510b77770cf
greb <greb>
2025-12-12 06:47:53 +0100

    Add day 12

---
+++ days/day12.py
@@ -0,0 +1,30 @@
+import re
+
+def parse(lines):
+    chunks = lines.split('\n\n')
+    *shapes, raw_regions = chunks
+
+    shapes = [s.splitlines()[1:] for s in shapes]
+    regions = []
+    for r in raw_regions.splitlines():
+        w,h,*s = map(int, re.findall(r'\d+', r))
+        regions.append((w,h,s))
+    return shapes, regions
+
+
+def part1(presents):
+    shapes, regions = presents
+
+    shapes_size = []
+    for shape in shapes:
+        size = sum(sum(c=='#' for c in l) for l in shape)
+        shapes_size.append(size)
+
+    count = 0
+    for w, h, region in regions:
+        # Exclude regions where total size of presents is already too large
+        size = sum(shapes_size[i]*r for i,r in enumerate(region))
+        if size > w*h:
+            continue
+        count += 1
+    return count
