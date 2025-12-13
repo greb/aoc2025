224508742e2a5f1f315771b96ba1581694ebc7e7
greb <greb>
2025-12-13 11:33:38 +0100

    Modify day 10 to remove external dependency
    * It's slower than the linear programming solution, but doesn't rely on
      external libraries.

--- days/day10.py
+++ days/day10.py
@@ -1,6 +1,6 @@
 import collections
 import itertools
-from scipy.optimize import linprog
+import functools
 
 
 Machine = collections.namedtuple('Machine', ['lights', 'buttons', 'joltage'])
@@ -8,10 +8,14 @@ Machine = collections.namedtuple('Machine', ['lights', 'buttons', 'joltage'])
 
 def parse_machine(line):
     tokens = line.split()
-    lights = [l == '#' for l in tokens[0][1:-1]]
+    lights = tuple(l == '#' for l in tokens[0][1:-1])
     buttons = [tuple(int(b) for b in bs[1:-1].split(',')) for
                      bs in tokens[1:-1]]
     joltage = tuple(int(j) for j in tokens[-1][1:-1].split(','))
+
+    # Turn buttons into an adjacency matrix for easier handling
+    buttons = [tuple(int(i in bs) for i in range(len(joltage)))
+               for bs in buttons]
     return Machine(lights, buttons, joltage)
 
 
@@ -19,30 +23,46 @@ def parse(lines):
     return [parse_machine(line) for line in lines.splitlines()]
 
 
-def press_buttons(machine):
+def check_lights(machine):
     # Tree search was too slow, even with a priority queue
     # Since a button is pressed only once at max (pressing twice does
     # nothing), it is enough to iterate through all button combinations
-    for n in range(1, len(machine.buttons)):
+    for n in range(1, len(machine.buttons)+1):
         for pressed in itertools.combinations(machine.buttons, n):
-            state = [sum(i in p for p in pressed)
-                     for i in range(len(machine.lights))]
-            state = [bool(i%2) for i in state]
-            if state == machine.lights:
+            state = tuple(sum(bs) for bs in zip(*pressed))
+            parity = tuple(bool(s%2) for s in state)
+            if parity == machine.lights:
                 return n
 
-
 def part1(machines):
-    return sum(map(press_buttons, machines))
+    return sum(check_lights(m) for m in machines)
 
-def find_min_joltage(machine):
-    c = [1 for _ in machine.buttons]
-    A = [[i in b for b in machine.buttons]
-         for i in range(len(machine.joltage))]
-    total = linprog(c, A_eq=A, b_eq=machine.joltage, integrality=1,
-                    options={'disp': False}).fun
-    return int(total)
+
+def check_joltage(machine):
+    costs = dict()
+    # Null cost needed when all parities are even
+    costs[(0,)*len(machine.joltage)] = 0
+    for n in range(1, len(machine.buttons)+1):
+        for pressed in itertools.combinations(machine.buttons, n):
+            state = tuple(sum(bs) for bs in zip(*pressed))
+            if state in costs:
+                continue
+            costs[state] = n
+
+    @functools.cache
+    def solve(joltage):
+        if all(j == 0 for j in joltage):
+            return 0
+        result = sum(joltage)
+        for state, cost in costs.items():
+            if not all(s <= j and s%2 == j%2 for s,j in zip(state, joltage)):
+                continue
+            new_joltage = tuple((j-s)//2 for s,j in zip(state, joltage))
+            result = min(result, cost + 2*solve(new_joltage))
+        return result
+
+    return solve(machine.joltage)
 
 
 def part2(machines):
-    return sum(map(find_min_joltage, machines))
+    return sum(check_joltage(m) for m in machines)
